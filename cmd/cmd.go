5c92e29c7cd7012a2ea82c9b7dc590ade038a7a6
Ali Shanaakh <ashanaakh@gmail.com>
2018-06-17 13:19:59 +0300

    Release first working version (#5)
    * Update cities storage
    
    * Release first working API

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,15 +1,24 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/ashanaakh/ua-cities/api"
 	"github.com/gin-gonic/gin"
 )
 
+// Run executes main project logic
 func Run() {
 	router := gin.Default()
+	api := api.NewCitiesAPI()
 
 	router.GET("/city/:name", api.HandleCity)
 
-	// TODO: Make port flexible.
-	router.Run(":8080")
+	port, ok := os.LookupEnv("PORT")
+
+	if !ok {
+		port = "8080"
+	}
+
+	router.Run(":" + port)
 }
